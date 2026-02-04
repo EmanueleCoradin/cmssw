3d6dfcfe68cc10d047b592e52a6b3a44dfdbea49
Emanuele Coradin <emanuele.coradin@studenti.unipd.it>
2026-02-04 18:04:00 +0100

    Improved inference code

--- HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2PixelTrackTorchHighPuritySelector_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2PixelTrackTorchHighPuritySelector_cfi.py
@@ -1,13 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
-hltPhase2PixelTrackFeatureExtractor = cms.EDProducer('PixelTrackFeaturesExtractor@alpaka',
+hltPhase2PixelTrackTorchHighPuritySelector = cms.EDProducer('PixelTrackTorchHighPuritySelector@alpaka',
     pixelRecHitSrc = cms.InputTag('hltPhase2PixelRecHitsExtendedSoA'),
     pixelTrackSrc = cms.InputTag('hltPhase2PixelTracksSoA'),
     maxNumberOfTracks = cms.int32(2*60*1024),
-    maxPreselectedTracks = cms.int32(15_000),
+    maxPreselectedTracks = cms.int32(4_096),
     minNumberOfHits = cms.int32(0),
     avgHitsPerTrack = cms.int32(8),
     minimumTrackQuality = cms.string('tight'),
-    model = cms.FileInPath('RecoTracker/FinalTrackSelectors/data/track_classifier_GPU.pt'),
-    scoreThreshold = cms.double(0.11)
+    model = cms.FileInPath('RecoTracker/FinalTrackSelectors/data/track_classifier_long_GPU.pt'),
+    scoreThreshold = cms.double(0.5)
 )
